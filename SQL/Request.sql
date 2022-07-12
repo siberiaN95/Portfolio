231dbe035f5b5863b17a90ff4051b57291714893
siberiaN95 <107300923+siberiaN95@users.noreply.github.com>
2022-07-12 22:00:28 +0300

    format

--- SQL/Request.sql
+++ SQL/Request.sql
@@ -2,5 +2,5 @@
 Вывести те компании, в которых меньше 100 автомобилей. Поле с числом автомобилей назвать cnt, 
 поле с названием компании- company_name.
 
-Select company_name , COUNT ( vehicle_id) AS cnt FROM cabs 
-GROUP BY company_name HAVING COUNT (vehicle_id<100) ORDER BY COUNT (vehicle_id) DESC;
+SELECT company_name, COUNT(vehicle_id) AS cnt FROM cabs 
+GROUP BY company_name HAVING COUNT (vehicle_id < 100) ORDER BY COUNT (vehicle_id) DESC;
